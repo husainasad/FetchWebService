682d789e793a3c74df9f4bda564671c94801070a
husainasad <asad1999@gmail.com>
2024-11-29 01:14:50 -0700

    implemented pattern validation

--- fetch_service.py
+++ fetch_service.py
@@ -1,79 +1,33 @@
 from fastapi import FastAPI, HTTPException, Path
-from pydantic import BaseModel, Field, field_validator, ValidationInfo
+from pydantic import BaseModel, Field
 from uuid import uuid4
 from typing import List, Dict
 import math
-from datetime import datetime
-from decimal import Decimal, InvalidOperation
+from datetime import date, time
+from decimal import Decimal
 
 app = FastAPI()
 
-AFTERNOON_START = datetime.strptime("14:00", "%H:%M").time()
-AFTERNOON_END = datetime.strptime("16:00", "%H:%M").time()
+AFTERNOON_START = time(14, 0)
+AFTERNOON_END = time(16, 0)
 PRICE_MULTIPLIER = Decimal("0.2")
 
 class Item(BaseModel):
-    shortDescription: str = Field(..., description="Short description of the item")
-    price: str = Field(..., description="Price of the item")
-
-    @field_validator("price")
-    def validate_price(cls, price_value):
-        try:
-            price = Decimal(price_value)
-            if price < 0:
-                # raise HTTPException(status_code=400, detail="Price must be greater than zero")
-                raise ValueError("Price must be greater than zero")
-            return price_value
-        except InvalidOperation:
-            # raise HTTPException(status_code=400, detail="Price must be a valid decimal number")
-            raise ValueError("Price must be a valid decimal number")
+    shortDescription: str = Field(..., pattern = "^[\\w\\s\\-]+$", description="The Short Product Description for the item.", example="Mountain Dew 12PK")
+    price: str = Field(..., pattern = "^\\d+\\.\\d{2}$", description="The total price payed for this item.", example="6.49")
 
 class Receipt(BaseModel):
-    retailer: str = Field(..., description="Name of the retailer")
-    purchaseDate: str = Field(..., description="Purchase date in YYYY-MM-DD format")
-    purchaseTime: str = Field(..., description="Purchase time in HH:MM format")
+    retailer: str = Field(..., pattern = "^[\\w\\s\\-&]+$", description="The name of the retailer or store the receipt is from.", example="Target")
+    purchaseDate: date = Field(..., description="The date of the purchase printed on the receipt.", example = "2022-01-01")
+    purchaseTime: time = Field(..., description="The time of the purchase printed on the receipt. 24-hour time expected.", example = "13:01")
     items: List[Item] = Field(..., min_items=1, description="List of items purchased")
-    total: str = Field(..., description="Total amount spent")
-
-    @field_validator("purchaseDate")
-    def validate_date(cls, purchaseDate_value):
-        try:
-            datetime.strptime(purchaseDate_value, "%Y-%m-%d")
-            return purchaseDate_value
-        except ValueError:
-            # raise HTTPException(status_code=400, detail="purchaseDate must be a valid input of format YYYY-MM-DD")
-            raise ValueError("purchaseDate must be a valid input of format YYYY-MM-DD")
-        
-    @field_validator("purchaseTime")
-    def validate_time(cls, purchaseTime_value):
-        try:
-            datetime.strptime(purchaseTime_value, "%H:%M").time()
-            return purchaseTime_value
-        except ValueError:
-            # raise HTTPException(status_code=400, detail="purchaseTime must be a valid input of 24 Hours format (HH:MM)")
-            raise ValueError("purchaseTime must be a valid input of 24 Hours format (HH:MM)")
-        
-    @field_validator("total")
-    def validate_total(cls, total_value, info: ValidationInfo) -> str:
-        try:
-            total = Decimal(total_value)
-            items = info.data.get("items")
-            if items is not None:
-                calculated_total = sum(Decimal(item.price) for item in items)
-                if total != calculated_total:
-                    # raise HTTPException(status_code=400, detail=f"Total ({total}) does not match the sum of item prices ({calculated_total})")
-                    raise ValueError(f"Total ({total}) does not match the sum of item prices ({calculated_total})")
-            return total_value
-        except InvalidOperation:
-            # raise HTTPException(status_code=400, detail="Total must be a valid decimal number")
-            raise ValueError("Total must be a valid decimal number")
-
+    total: str = Field(..., pattern = "^\\d+\\.\\d{2}$", description="The total amount paid on the receipt.", example = "35.35")
 
 class ReceiptResponse(BaseModel):
-    id: str = Field(..., description="Unique ID assigned to the receipt")
+    id: str = Field(..., pattern = "^\\S+$", description="Returns the ID assigned to the receipt", example = "adb6b560-0eef-42bc-9d16-df48f30e89b2")
 
 class PointsResponse(BaseModel):
-    points: int = Field(..., description="Points awarded for the receipt")
+    points: int = Field(..., description="The number of points awarded", example = 100)
 
 def calculate_points(receipt: Receipt) -> int:
     points = 0
@@ -102,13 +56,11 @@ def calculate_points(receipt: Receipt) -> int:
             points += math.ceil(item_price * PRICE_MULTIPLIER)
 
     # Rule 6: 6 points if the day in the purchase date is odd
-    purchase_date = datetime.strptime(receipt.purchaseDate, "%Y-%m-%d")
-    if purchase_date.day % 2 != 0:
+    if receipt.purchaseDate.day % 2 != 0:
         points += 6
 
     # Rule 7: 10 points if the time of purchase is after 2:00pm and before 4:00pm
-    purchase_time = datetime.strptime(receipt.purchaseTime, "%H:%M").time()
-    if AFTERNOON_START <= purchase_time < AFTERNOON_END:
+    if AFTERNOON_START <= receipt.purchaseTime < AFTERNOON_END:
         points += 10
 
     return points
@@ -117,20 +69,19 @@ receipts_db: Dict[str, Receipt] = {}
 points_db: Dict[str, int] = {}
 
 @app.post("/receipts/process", response_model=ReceiptResponse, status_code=200)
-async def process_receipt(receipt: Receipt):
+async def process_receipt(receipt: Receipt):    
     receipt_id = str(uuid4())
-
     receipts_db[receipt_id] = receipt.model_dump()
     points_db[receipt_id] = calculate_points(receipt)
 
     return {"id": receipt_id}
 
 @app.get("/receipts/{id}/points", response_model=PointsResponse, status_code=200)
-async def get_points(id: str = Path(..., description="Receipt ID")):
+async def get_points(id: str = Path(..., pattern = "^\\S+$", description="Receipt ID")):
     id = id.strip('"')
 
     if id not in points_db:
-        raise HTTPException(status_code=404, detail="No receipt found for given id")
+        raise HTTPException(status_code=404, detail="No receipt found for that id")
     
     points = points_db[id]
 
